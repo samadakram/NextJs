dd080d409d461d15f5753f3c8aa170006ee37c13
Abdul Samad <as765722@gmail.com>
2023-09-20 15:14:44 +0500

    Fetching Data Dynamically
    Added file CustomerData in json format and fetching and displaying data dynamically

--- pages/customers/[customerId]/projects/index.js
+++ pages/customers/[customerId]/projects/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router"
 
@@ -6,17 +7,48 @@ function Projects() {
     const router = useRouter();
     const { customerId, projectId } = router.query;
 
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        getCustomerData();
+    }, []);
+
+    const getCustomerData = async () => {
+        try {
+            const response = await fetch("/customerData.json");
+            const data = await response.json();
+            data.customers.forEach(element => {
+                let route = element.Route;
+                if (route === customerId) {
+                    setProjects(element.projects);
+                }
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
         <div>
             <h1>{customerId}'s Projects List Page</h1>
             <h2>Projects</h2>
             <ul>
-                <li>
+                {
+                    projects.map((project, index) => {
+                        return <li key={index}>
+                            <Link href={`/customers/${customerId}/projects/${project}`}>{project}</Link>
+                        </li>
+                    })
+                }
+
+
+
+                {/* <li>
                     <Link href={`/customers/${customerId}/projects/trackingApp`}>Tracking App</Link>
                 </li>
                 <li>
                     <Link href={`/customers/${customerId}/projects/portfolio`}>Portfolio</Link>
-                </li>
+                </li> */}
             </ul>
         </div>
     )
--- pages/customers/index.js
+++ pages/customers/index.js
@@ -1,16 +1,45 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function Customers() {
+
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        getCustomerData();
+    }, []);
+
+    const getCustomerData = async () => {
+        try {
+            const response = await fetch("/customerData.json");
+            const data = await response.json();
+            setCustomers(data.customers);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
         <div>
             <h1>Customers Page</h1>
             <ul>
-                <li>
+                {
+                    customers.map((customer, index) => {
+                        return (
+                            <li>
+                                <Link href={`customers/${customer.Route}`}>{customer.Name}</Link>
+                            </li>
+                        )
+                    })
+                }
+
+                {/* <li>
                     <Link href="customers/ishaq">Ishaq</Link>
                 </li>
                 <li>
                     <Link href="customers/samad">Samad</Link>
-                </li>
+                </li> */}
             </ul>
         </div>
     )
